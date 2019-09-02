173b39e48d4eab0ea3c0f0bbe739518c96f2c68b
Manoel Neto <tremyen@gmail.com>
2019-09-02 14:51:55 -0300

    Acerto no BUG de limpar a frase a cada execucao

--- main.asm
+++ main.asm
@@ -9,13 +9,14 @@ PrintChar	equ &BB5A
 org &8000
 	call GetMsg 		; obtem a mensagem do usuario
 	call NewLine		; pula uma linha
-	call MessMsg		; troca a mensagem
+	call MessMsg		; bagunca a mensagem
 	;call NewLine
 	;call WaitPlayer
 	;call ShowErroCount
 ret
 
 GetMsg:
+	call LimpaString	; limpa a string a cada execucao
 	ld hl,Frase		; carrega o endereco de memoria onde
 				; a frase sera armazenada
 	ld b,0			; zera o contador de letras
@@ -32,20 +33,18 @@ loopGM:
 	inc b			; aumenta o contador de letras
 	cp 13			; compara o carcter entrado com o ENTER(13)
 				; se o usuario apertou enter, a frase acabou	
+	ld a,b			; coloca o contador de letras no acumulador
 	jp z,ValidaDuasLetras	; se a frase terminou por enter precisamos 
 				; validar se temos pelo menos duas letras
-Validado:	
 	cp 14			; compara o contador com 14
 	ret z			; se A-14 = 0 vc ja digitou 14 letras
 jp loopGM
 
-ValidaDuasLetras:
-	ld a,b			; coloca o contador de letras no acumulador
-				; para a comparacao
+ValidaDuasLetras:		
 	cp 3			; compara com 3, pois o enter eh um caracter
 	ret nc			; se a>=2 esta ok, retorna
-	jp Validado		; senao volta para o loop de recebimento
-
+	call LimpaString	; senao limpa a string
+	jp loopGM		; volta para receber a string novamente
 MessMsg:
 	ld hl, Frase+13
 	ld b,0
@@ -63,6 +62,22 @@ WaitPlayer:
 ret
 
 ShowErroCount:
+ret
+
+LimpaString:
+	ld hl,Frase 		; carrega o endereco de memoria da frase
+	ld c,0			; carrega o contador do loop
+InicioLimpeza:	
+	ld (hl),32		; bota um espaco na posicao
+	inc hl			; prepara proxima posicao
+	ld a,c			; bota o contador no acumulador
+	cp 14			; testa se ja limpou 14 letras
+	inc c			; incrementa contador do loop
+	jp z,FimLimpeza		; se sim termina a limpeza
+	jp InicioLimpeza	; senao limpa a proxima posicao
+FimLimpeza:
+	ld hl,Frase		; prepara HL com o endereco da frase
+	ld b,0			; zera o contador de letras
 ret
 
 NewLine:
