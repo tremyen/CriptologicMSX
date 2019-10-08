b9c4adbf986d31d6e0915205f8f8500a8eb7cdf7
Manoel Neto <tremyen@gmail.com>
2019-10-08 16:16:32 -0300

    Nova Arquitetura

---
+++ MSX/Exemplos/Constantes.asm
@@ -0,0 +1,19 @@
+; =============================================================================
+; Constantes.asm
+; =============================================================================
+; Manoel Neto 2019-10-3
+; Constantes utilizadas no Criptologic Z80
+; =============================================================================
+NumPosXCabecalho:     equ 3
+NumPosYCabecalho:     equ 2
+NumPosXMensagens:     equ 1
+NumPosYMensagens:     equ 6
+NumPosXErros:         equ 1
+NumPosYErros:         equ 7
+NumPosXEntradas:      equ 7
+NumPosYEntrada1:      equ 13
+NumPosYEntrada2:      equ 15
+NumPosXLinhaApoio:    equ 7
+NumPosYLinhaApoio1:   equ 14
+NumPosYLinhaApoio2:   equ 16
+NumPosYMensagemFinal: equ 23
---
+++ MSX/Exemplos/Strings.asm
@@ -0,0 +1,24 @@
+; =============================================================================
+; Strings.asm
+; =============================================================================
+; Manoel Neto 2019-09-25
+; Strings utilizadas no Criptologic Z80
+; =============================================================================
+MsgUsuario1:
+	db "Criptologic 1.0 para MSX",13
+MsgUsuario2:
+	db "Digite uma frase e <ENTER>",13
+MsgUsuario3:
+		db "Voce so cometeu ",13
+MsgUsuario4:
+		db " erros.",13
+MsgUsuario5:
+	db "Esta Correto! 8-)",13
+MsgUsuario6:
+	db "Esta Errado. 8-(",13
+MsgUsuario7:
+	db "Parabens! Acertou tudo!",13
+MsgUsuario8:
+	db "Pressione <ENTER> para jogar",13
+LinhaApoio:
+	db "______________",13
