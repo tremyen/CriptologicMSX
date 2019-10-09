6d79857db95f96343d76c9614c771d8c775a0034
Manoel Neto <tremyen@gmail.com>
2019-10-09 18:19:46 -0300

    Gravar na memoria VRAM

--- MSX/Exemplos/ImprimirFonte.asm
+++ MSX/Exemplos/ImprimirFonte.asm
@@ -22,17 +22,57 @@ org romArea
 ; INICIO PROGRAMA
 ; =============================================================================
 startCode:
+ 	call DISSCR             ; desligo a exibição da tela
+	call ERAFNK             ; desligo as teclas de função
+  ld a,13
+  ld (FORCLR),a           ; cor da frente em magenta
+  ld a,1                  ; Preto
+  ld (BAKCLR),a           ; cor de fundo
+  ld (BDRCLR),a           ; cor da borda
+  call CHGCLR             ; agora mudo as cores da tela
+	ld a,32
+  ld (LINL32),a           ; largura da tela em 32 colunas
 	call ScreenINIT
-	; Escrever no enredeco 1600(H) tabela de padroes de sprites
-	; ld a,%00010110
-	; ld h,%00000000
-	; ld l,%00000000
-	; call SetVDP_Write
-	; ld hl,Font
-	; call DoCopy
+
+	; ===========================================================================
+	; Carrega a Tabela de padroes
+	; ===========================================================================
+	ld bc,24             		; bytes a copiar
+  ld de,0                 ; tabela de padrões na VRAM
+  ld hl,CriptoFont 				; localização na RAM
+  call LDIRVM             ; copio a tabela de padrões
+
+  ; ld bc,2048              ; bytes a copiar
+  ; ld de,8192              ; tabela de atributos na VRAM
+  ; ld hl,charAttributes    ; localização na RAM
+  ; call LDIRVM             ; copio a tabela de atributos
+
+  ; ld bc,8               	; bytes a copiar
+  ; ld de,14336             ; tabela de sprites na VRAM
+  ; ld hl,spritePatterns    ; localização na RAM
+  ; call LDIRVM             ; copio a tabela de sprites
+
+	; ===========================================================================
+	; Carrega padroes na tela
+	; ===========================================================================
+	ld bc,14                ; bytes a copiar
+  ld de,6144+14           ; posição na tela
+  ld hl,LinhaReta14	      ; padrão da string
+  call LDIRVM             ; copio na VRAM
+
+	ld bc,14                ; bytes a copiar
+  ld de,6144+7            ; posição na tela
+  ld hl,LinhaReta14	      ; padrão da string
+  call LDIRVM             ; copio na VRAM
+
+  call ENASCR             ; religo a tela
+
 LoopInfinito:
 	jr LoopInfinito
 ret
+
+LinhaReta14:
+  db 0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
 ; =============================================================================
 ; FIM PROGRAMA
