1691a000d059968179aa6d11d7b93efc098cec58
Manoel Neto <tremyen@gmail.com>
2019-10-19 22:29:23 -0300

    VOltando para a versão ROM

--- MSX/Cripto.asm
+++ MSX/Cripto.asm
@@ -9,15 +9,14 @@ include "Hardware\BiosMSX.asm"
 include "Assets\Constantes.asm"
 include "Assets\Variaveis.asm"
 
-; org romArea
-; 	db "AB"                     ; identifica como ROM
-;   dw startCode                ; endereço de execução
-;   db "CW01"                   ; string de identificação
-;   db __VERSION+48							; cria o identificador de versao
-;   db __RELEASE+65							; cria o identificador da release
-;   ds 6,0
+ org romArea
+ 	db "AB"                     ; identifica como ROM
+   dw startCode                ; endereço de execução
+   db "CW01"                   ; string de identificação
+   db __VERSION+48							; cria o identificador de versao
+   db __RELEASE+65							; cria o identificador da release
+   ds 6,0
 
-org &810A
 ; =============================================================================
 ; INICIO PROGRAMA
 ; =============================================================================
@@ -55,6 +54,6 @@ include "Assets\Sprites.asm"
 ; =============================================================================
 ; Padding
 ; =============================================================================
-; romPad:
-;   ds romSize-(romPad-romArea),0
-; end
+ romPad:
+   ds romSize-(romPad-romArea),0
+ end
